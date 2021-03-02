e1a488a2a6b7d35d905c77a13c0ca1b3ff5e8d6a
gianmarco.carnemolla <gianmarco.carnemolla@ttsolutions.it>
2021-03-02 18:15:44 +0100

    modifica demo service

--- src/main/resources/static/app/service/demoService.js
+++ src/main/resources/static/app/service/demoService.js
@@ -3,13 +3,15 @@
 angular.module('demoApp')
     .service('DemoService', DemoService);
 
-DemoService.$inject = ['$http', '$resource'];
+DemoService.$inject = ['$http'];
 
-function DemoService($http, $resource) {
+function DemoService($http) {
     var service = this;
 
     service.getCompany = function (id) {
-        return $resource('/api/:id', {}, { get: { method: 'GET', params: { id: '@id' } } });
+        return $http.get('/api/' + id).then(function (response) {
+            return response.data.company;
+        });
     };
     service.getPlate = function (id) {
         return $http.get('/api/' + id).then(function (response) {
